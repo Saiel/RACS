68f07d12933e453b1cb129b2dc95d9dc304509a1
Zaytsev Alex <zaicevalex2000@gmail.com>
2019-10-16 13:49:15 +0300

    Fixed bug when unknown hash is gotten to lock-api Lowered hash, that sends as argument in get_instance_by_hash

--- src/lockadmin/views/locks_views.py
+++ src/lockadmin/views/locks_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from datetime import datetime
@@ -9,10 +10,13 @@ from ..models import Accesses, Locks, Logs, UserModel
 
 @api_view(['GET'])
 def check_access(request: Request):
-    lock_id_hash = request.query_params['lock']
-    user_id_hash = request.query_params['pass']
-    user = UserModel.get_instance_by_hash_id(user_id_hash)
-    lock = Locks.get_instance_by_hash_id(lock_id_hash)
+    lock_id_hash: str = request.query_params['lock']
+    user_id_hash: str = request.query_params['pass']
+    try:
+        user = UserModel.get_instance_by_hash_id(user_id_hash.lower())
+        lock = Locks.get_instance_by_hash_id(lock_id_hash.lower())
+    except ObjectDoesNotExist as exc:
+        return Response(0, headers={'Error': str(exc)})
     now  = datetime.utcnow()
     result = Accesses.objects.filter(user=user, lock=lock, access_start__lte=now, access_stop__gte=now).exists()
     Logs.objects.create(user=user, lock=lock, result=result, try_time=now)
