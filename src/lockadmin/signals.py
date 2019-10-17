32657e7b7500e247a3111140abe922d2b7a0e533
Zaytsev Alex <zaicevalex2000@gmail.com>
2019-10-17 11:51:38 +0300

    Make card_id in user model case-insensitive (now it saves always in uppercase and then hash)

--- src/lockadmin/signals.py
+++ src/lockadmin/signals.py
@@ -15,10 +15,10 @@ def hash_id(sender, **kwargs):
     new_lock.save()
 
 
-@receiver(post_save, sender=UserModel)
+@receiver(pre_save, sender=UserModel)
 def hash_id(sender, **kwargs):
     new_user = kwargs['instance']
+    new_user.card_id = new_user.card_id.upper()
     if new_user.hash_id:
         return
     new_user.hash_id = sha1(str(new_user.card_id).encode('utf-8')).hexdigest()
-    new_user.save()
