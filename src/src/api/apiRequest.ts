64bea2b856cde60dbd91e80a8083435202a51981
AlexKale <alex-kale@hotmail.com>
2019-10-18 20:19:55 +0300

    add linter config files

--- src/src/api/apiRequest.ts
+++ src/src/api/apiRequest.ts
@@ -1,26 +1,17 @@
-import {
-  User,
-  Log,
-  Lock,
-  LockAccess,
-  UserPOST,
-  Role,
-  LockAccessPOST,
-  LockPOST
-} from "store/types";
+import { User, Log, Lock, LockAccess, UserPOST, Role, LockAccessPOST, LockPOST } from 'store/types';
 
-export const API_HOST = "/";
-export const API_BASE = "api/v1/";
+export const API_HOST = '/';
+export const API_BASE = 'api/v1/';
 
 // TO-DO: move to separate files
 
 export const codeToMessage: Record<number, string> = {
-  400: "Некорректный запрос",
-  404: "Ресурс не найден",
-  500: "Ошибка на сервере"
+  400: 'Некорректный запрос',
+  404: 'Ресурс не найден',
+  500: 'Ошибка на сервере',
 };
 
-export type Models = "users" | "logs" | "locks" | "accesses" | "roles";
+export type Models = 'users' | 'logs' | 'locks' | 'accesses' | 'roles';
 
 export type POSTModels = {
   users: UserPOST;
@@ -46,11 +37,11 @@ export type APIResponse<T> = {
 };
 
 export type APIResponses = {
-  users: APIResponse<ModelTypes["users"]>;
-  logs: APIResponse<ModelTypes["logs"]>;
-  locks: APIResponse<ModelTypes["locks"]>;
-  accesses: APIResponse<ModelTypes["accesses"]>;
-  roles: APIResponse<ModelTypes["roles"]>;
+  users: APIResponse<ModelTypes['users']>;
+  logs: APIResponse<ModelTypes['logs']>;
+  locks: APIResponse<ModelTypes['locks']>;
+  accesses: APIResponse<ModelTypes['accesses']>;
+  roles: APIResponse<ModelTypes['roles']>;
 };
 
 export function getAPIUrl(endpoint: string) {
@@ -65,7 +56,7 @@ export function apiRequest(url: string, params: RequestInit = {}) {
     let status = 0;
 
     fetch(`${url}/`, params)
-      .then(response => {
+      .then((response) => {
         status = response.status;
 
         if (status === 204) {
@@ -75,22 +66,17 @@ export function apiRequest(url: string, params: RequestInit = {}) {
           return response.json();
         }
 
-        const contentType = response.headers.get("content-type");
+        const contentType = response.headers.get('content-type');
 
-        if (contentType && contentType.indexOf("application/json") !== -1) {
-          return response.json().then(json => reject({ ...json, status }));
+        if (contentType && contentType.indexOf('application/json') !== -1) {
+          return response.json().then((json) => reject({ ...json, status }));
         }
 
-        throw new Error(
-          codeToMessage[status]
-            ? codeToMessage[status]
-            : `Ошибка: HTTP ${status}`
-        );
+        throw new Error(codeToMessage[status] ? codeToMessage[status] : `Ошибка: HTTP ${status}`);
       })
       .then((json: Object) => resolve({ ...json, status }))
-      .catch(error => {
-        if (typeof error === "undefined")
-          reject(new Error("Ошибка при выполнении запроса"));
+      .catch((error) => {
+        if (typeof error === 'undefined') reject(new Error('Ошибка при выполнении запроса'));
         if (error.message) {
           reject(error);
         } else {
@@ -100,23 +86,20 @@ export function apiRequest(url: string, params: RequestInit = {}) {
   });
 }
 
-export function apiDeleteRequest<T extends Models>(
-  endpoint: T,
-  id: number
-): Promise<Object> {
+export function apiDeleteRequest<T extends Models>(endpoint: T, id: number): Promise<Object> {
   return new Promise((resolve, reject) => {
     const url = getAPIUrl(`${endpoint}/${id}`);
 
-    apiRequest(url.href, { method: "DELETE" })
-      .then(json => resolve(json as Object))
-      .catch(error => reject(error));
+    apiRequest(url.href, { method: 'DELETE' })
+      .then((json) => resolve(json as Object))
+      .catch((error) => reject(error));
   });
 }
 
 // TO-DO: fix unnecessary redirect
 export function apiGetRequest<T extends Models>(
   endpoint: T,
-  params: Record<string, string | number> = {}
+  params: Record<string, string | number> = {},
 ): Promise<APIResponses[T]> {
   return new Promise((resolve, reject) => {
     const url = getAPIUrl(endpoint);
@@ -125,27 +108,27 @@ export function apiGetRequest<T extends Models>(
       url.search += `${param}=${value}&`;
     }
 
-    apiRequest(url.href, { mode: "cors" })
-      .then(json => resolve(json as APIResponses[T]))
-      .catch(error => reject(error));
+    apiRequest(url.href, { mode: 'cors' })
+      .then((json) => resolve(json as APIResponses[T]))
+      .catch((error) => reject(error));
   });
 }
 
 export function apiPostRequest<T extends Models>(
   endpoint: T,
-  data: POSTModels[T]
+  data: POSTModels[T],
 ): Promise<ModelTypes[T]> {
   return new Promise((resolve, reject) => {
     const url = getAPIUrl(endpoint);
-    console.log("POST data", data);
+    console.log('POST data', data);
     apiRequest(url.href, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-      .then(json => resolve(json as ModelTypes[T]))
-      .catch(error => reject(error));
+      .then((json) => resolve(json as ModelTypes[T]))
+      .catch((error) => reject(error));
   });
 }
