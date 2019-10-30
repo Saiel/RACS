0663b176f37693316502c6f7be73efee7adb6ddc
Alex Kale <alex-kale@github.com>
2019-10-30 18:50:22 +0300

    user

--- src/src/components/App/App.tsx
+++ src/src/components/App/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom'; 
 
 import Home from 'components/Home/Home';
 
 const App = () => (
-  <Home />
+  <HashRouter>
+    <Switch>
+      <Route path="/" exact={true}>
+        <Home />
+      </Route>
+    </Switch>
+  </HashRouter>
 );
 
 export default App;
