4f894ff062b0c37ffbf10f4c1ef9587c0dda4376
Alex Kale <alex-kale@github.com>
2019-11-06 00:30:56 +0300

    update header and layout

--- src/src/components/App/App.tsx
+++ src/src/components/App/App.tsx
@@ -5,17 +5,17 @@ import './root.scss';
 import 'components/Typo/Typo.css';
 import 'components/Layout/Layout.scss';
 import Home from 'components/Home/Home';
+import Header from 'components/Header/Header';
 
 const App = () => (
-  <div className="Page Typo">
-    <HashRouter>
-      <Switch>
-        <Route path="/" exact={true}>
-          <Home />
-        </Route>
-      </Switch>
-    </HashRouter>
-  </div>
+  <HashRouter>
+    <Header username="Александр Калентьев" userid={0} />
+    <Switch>
+      <Route path="/" exact={true}>
+        <Home />
+      </Route>
+    </Switch>
+  </HashRouter>
 );
 
 export default App;
