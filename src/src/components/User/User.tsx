8f299136761b192cb487fab77fe0d2a1462e2bd3
Alex Kale <alex-kale@github.com>
2019-11-06 01:57:28 +0300

    update AccessList and layouts

--- src/src/components/User/User.tsx
+++ src/src/components/User/User.tsx
@@ -5,6 +5,7 @@ import { API_STR } from 'api/apiRequest';
 
 import './User.scss';
 import Card from 'components/Card/Card';
+import AccessList from 'components/AccessList/AccessList';
 
 interface UserRoute {
   uId: string;
@@ -12,6 +13,7 @@ interface UserRoute {
 
 const User: React.FC<RouteComponentProps<UserRoute>> = ({ match: { params: { uId } } }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [userAccessList, setUserAccessList] = useState<LockAccess[]>([]);
 
   const userName = useMemo(() => {
     return user ? `#${user.u_id}: ${user.first_name} ${user.last_name}` : '';
@@ -25,16 +27,26 @@ const User: React.FC<RouteComponentProps<UserRoute>> = ({ match: { params: { uId
       setUser(json);
     }
 
+    async function getUserAccessList() {
+      const response = await fetch(`${API_STR}accesses/?u_id=${uId}`);
+      const json = await response.json();
+      console.log(json.results);
+      setUserAccessList(json.results);
+    }
+
     getUser();
+    getUserAccessList();
   }, [uId]);
 
   return (
     <div className="User Layout Layout_columns_2">
+      <div className="Page-Title">Пользователь {userName}</div>
       <div className="Layout-Column User-UserInfo">
-        {user && <div className="Page-Title">Пользователь {userName}</div>}
         {user && <Card><UserInfo user={user} /></Card>}
       </div>
       <div className="Layout-Columns User-Tables">
+        <div className="Layout-Title">Доступы пользователя</div>
+        <AccessList accesses={userAccessList} className="Layout-Table" />
       </div>
     </div>
   );
