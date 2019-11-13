5c8143561779ef3f903e4edb6814e3d01c730b74
Alex Kale <alex-kale@github.com>
2019-11-13 21:44:35 +0300

    add user_fio and lock_desc to LogsSerializer

--- src/src/components/Home/Home.tsx
+++ src/src/components/Home/Home.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-
+import usePagination from 'hooks/usePagination';
 import UserInfo from 'components/UserInfo/UserInfo';
 import LockList from 'components/LockList/LockList';
 import UserList from 'components/UserList/UserList';
-import { apiGetRequest } from 'api/apiRequest';
+import { apiGetRequest, APIResponse } from 'api/apiRequest';
 
 import './Home.scss';
 
+
 const Home = () => {
-  const [locks, setLocks] = useState<Lock[]>([]);
-  const [users, setUsers] = useState<User[]>([]);
+  const [locks, setLocks] = useState<APIResponse<Lock> | null>(null);
+  const [users, setUsers] = useState<APIResponse<User> | null>(null);
+  const usersPagination = usePagination(users, setUsers);
+  const locksPagination = usePagination(locks, setLocks); 
 
   useEffect(() => {
     apiGetRequest('users')
     .then(json => {
-      setUsers(json.results);
+      setUsers(json);
     })
 
     apiGetRequest('locks')
     .then(json => {
-      setLocks(json.results);
+      setLocks(json);
     })
 
   }, []);
@@ -31,15 +34,19 @@ const Home = () => {
         <div className="Layout-Title">
           Текущий пользователь
         </div>
-        { users.length > 0 && 
-          <UserInfo user={users[0]}/> 
+        { users && users.results.length > 0 && 
+          <UserInfo user={users.results[0]}/> 
         }
       </div>
       <div className="Layout-Column Home-Tables">
         <div className="Layout-Title">Аудитории</div>
-        <LockList locks={locks} className="Layout-Table" />
+        {locks && <LockList locks={locks.results} className="Layout-Table" />}
+        <button className="Page-Prev" onClick={locksPagination('previous')} > {'<'} </button>
+        <button className="Page-Next" onClick={locksPagination('next')}> > </button>        
         <div className="Layout-Title">Пользователи</div>
-        <UserList users={users} className="Layout-Table" />
+        {users && <UserList users={users.results} className="Layout-Table" />}
+        <button className="Page-Prev" onClick={usersPagination('previous')} > {'<'} </button>
+        <button className="Page-Next" onClick={usersPagination('next')}> > </button>
       </div>
     </div>
   );
