06360b051919bddc0a7258bbb24264336e01ac3f
Alex Kale <alex-kale@github.com>
2019-11-13 15:22:59 +0300

    change default page size

--- src/lockadmin/paginations.py
+++ src/lockadmin/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 15 
     page_size_query_param = 'count'
     max_page_size = 100
