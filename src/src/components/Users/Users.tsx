c219f0d740bb9484e1bd25d5cbf38a3ad9acbeb3
Alex Kale <alex-kale@github.com>
2019-11-17 20:04:23 +0300

    update list pages

--- src/src/components/Users/Users.tsx
+++ src/src/components/Users/Users.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { APIResponse } from 'api/apiRequest';
-import { getUsers, apiPost, API } from 'api';
+import { getUsers, apiPost } from 'api';
 import UserList from 'components/UserList/UserList';
 import usePagination from 'hooks/usePagination';
 
@@ -8,12 +8,20 @@ import './Users.scss';
 import UserForm from 'components/Forms/UserForm';
 import Overlay from 'components/Overlay/Overlay';
 import { RouteComponentProps } from 'react-router';
-import Pagination from 'components/Pagination/Pagination';
 
 const Users: React.FC<RouteComponentProps> = ({ history }) => {
   const [users, setUsers] = useState<APIResponse<User> | null>(null);
   const [addUserOverlayActive, setAddUserOverlayActive] = useState<Boolean>(false);
-
+  const toggleOverlay = useCallback(
+    (type: 'close' | 'open') => () => {
+      setAddUserOverlayActive(type === 'open');
+    },
+    [setAddUserOverlayActive],
+  );
+  const onUserFormSubmit = useCallback(async (state: FormData) => {
+    const json = await apiPost<User>('users', state);
+    history.push(`/users/${json.u_id}`);
+  }, []);
   const userPagination = usePagination(users, setUsers);
 
   const toggleOverlay = useCallback(
@@ -44,19 +52,26 @@ const Users: React.FC<RouteComponentProps> = ({ history }) => {
   }, []);
 
   return (
-    <div className="Users Layout">
-      <div className="Page-Title">Список пользователей</div>
-      <div className="Page-Actions">
-        <button className="Btn Btn_add" onClick={toggleOverlay('open')}>
-          Добавить пользователя
-        </button>
-      </div>
-      {users &&
-        <>
-          <UserList onDelete={deleteUser} users={users.results} />
-          <Pagination paginationFn={userPagination} state={users} />
-        </>
+    <div className="Users">
+      <div>Список пользователей</div>
+      {users && 
+      <>
+        <UserList users={users.results} />
+        <div className="Pageination">
+          <button disabled={!users.previous} className="Pagination-Prev" onClick={userPagination('previous')}>
+            {' '}
+            {'<'}{' '}
+          </button>
+          <button disabled={!users.next} className="Pagination-Next" onClick={userPagination('next')}>
+            {' '}
+            >{' '}
+          </button>
+        </div>
+      </>
       }
+      <button className="Btn Btn_add" onClick={toggleOverlay('open')}>
+        Добавить пользователя
+      </button>
       {addUserOverlayActive && (
         <Overlay onClose={toggleOverlay('close')}>
           <UserForm onSubmit={onUserFormSubmit}></UserForm>
