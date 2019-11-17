0be05b1884d95fb2411139a6288dc57638aa9124
Alex Kale <alex-kale@github.com>
2019-11-17 20:04:23 +0300

    update list pages

--- src/src/components/UserList/UserList.tsx
+++ src/src/components/UserList/UserList.tsx
@@ -15,7 +15,6 @@ const UserListRow: React.FC<{ user: User; onDelete?: deleteFn }> = ({ user, onDe
       <div className="User-Email">{user.email}</div>
       <div className="User-CardID">{user.card_id}</div>
       <div className="User-Role">{user.role}</div>
-      { onDelete && <button onClick={(e) => { e.preventDefault(); onDelete(user.u_id); }} className="Btn Btn_danger">Удалить</button>}
     </React.Fragment>
   )
 };
@@ -26,22 +25,22 @@ interface UserListProps {
   onDelete?: deleteFn;
 }
 
-const UserList: React.FC<UserListProps> = ({ users, className = '', onDelete }) => {
+const UserList: React.FC<UserListProps> = ({ users, className = '' }) => {
   return (
     <div className={`UserList ${className}`}>
       <div className="Table">
         <div className="Table-Row Table-Header">
-          {userFields.map((field, idx) =>
-            <div className="Table-Item" key={idx}>{field}</div>
-          )}
+          { userFields.map((field, idx) => 
+              <div className="Table-Item" key={idx}>{field}</div>
+            ) }
         </div>
-        {users.map((user) => {
-          return (
-            <Link className="BlockLink BlockLink_primary" to={`/users/${user.u_id}`} key={user.u_id}><div className="Table-Row">
-              <UserListRow onDelete={onDelete} user={user} />
-            </div></Link>
-          )
-        })
+        { users.map((user) => { 
+            return (
+              <Link className="BlockLink BlockLink_primary" to={`/users/${user.u_id}`}><div className="Table-Row" key={user.u_id}>
+                <UserListRow user={user} />
+              </div></Link>
+            )
+          }) 
         }
       </div>
     </div>
