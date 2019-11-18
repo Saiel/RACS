3b7e15fd0ecfabd5a38566114ccd1b889bdb592a
Alex Kale <alex-kale@github.com>
2019-11-18 15:07:21 +0300

    delete handlers, pagination, minor fixes

--- src/src/components/AccessList/AccessList.tsx
+++ src/src/components/AccessList/AccessList.tsx
@@ -11,9 +11,15 @@ const accessFields = [
 interface AccessListProps {
   accesses: LockAccess[];
   className?: string;
+  onDelete?: deleteFn;
 }
 
-const AccessInfoRow: React.FC<LockAccess> = ({ lock_desc, user_fio, access_start, access_stop }) => {
+interface AccessInfoRowProps {
+  access: LockAccess;
+  onDelete?: deleteFn; 
+}
+
+const AccessInfoRow: React.FC<AccessInfoRowProps> = ({ access: { a_id, lock_desc, user_fio, access_start, access_stop }, onDelete }) => {
   const lockDesc = useMemo(() => lock_desc.split('\r\n')[0], [lock_desc]);
   const accessStart = useMemo(() => new Date(access_start).toLocaleString(), [access_start]);
   const accessStop = useMemo(() => new Date(access_stop).toLocaleString(), [access_stop]);
@@ -25,13 +31,14 @@ const AccessInfoRow: React.FC<LockAccess> = ({ lock_desc, user_fio, access_start
       <div className="Access-User">{user_fio}</div>
       <div className="Access-Start">{accessStart}</div>
       <div className="Access-Stop">{accessStop}</div>
+      { onDelete && <button onClick={() => onDelete(a_id)} className="Btn Btn_danger Access-Delete">Удалить</button> }
     </React.Fragment>
   )
 }
 
-const AccessList: React.FC<AccessListProps> = ({ accesses, className = '' }) => {
+const AccessList: React.FC<AccessListProps> = ({ accesses, className = '', onDelete }) => {
   const accessRows = accesses.map((access) => 
-    <AccessInfoRow key={access.a_id} {...access} />
+    <AccessInfoRow key={access.a_id} access={access} onDelete={onDelete} />
   );
 
   return (
--- src/src/components/UserList/UserList.tsx
+++ src/src/components/UserList/UserList.tsx
@@ -6,7 +6,7 @@ const userFields = [
   'ID', 'Имя', 'Фамилия', 'Email', 'ID карточки', 'Роль',
 ];
 
-const UserListRow: React.FC<{ user: User }> = ({ user }) => {
+const UserListRow: React.FC<{ user: User; onDelete?: deleteFn }> = ({ user, onDelete }) => {
   return (
     <React.Fragment>
       <div className="User-ID">{user.u_id}</div>
@@ -15,6 +15,7 @@ const UserListRow: React.FC<{ user: User }> = ({ user }) => {
       <div className="User-Email">{user.email}</div>
       <div className="User-CardID">{user.card_id}</div>
       <div className="User-Role">{user.role}</div>
+      { onDelete && <button onClick={(e) => { e.preventDefault(); onDelete(user.u_id); }} className="Btn Btn_danger">Удалить</button>}
     </React.Fragment>
   )
 };
@@ -22,24 +23,25 @@ const UserListRow: React.FC<{ user: User }> = ({ user }) => {
 interface UserListProps {
   users: User[];
   className?: string;
+  onDelete?: deleteFn;
 }
 
-const UserList: React.FC<UserListProps> = ({ users, className = '' }) => {
+const UserList: React.FC<UserListProps> = ({ users, className = '', onDelete }) => {
   return (
     <div className={`UserList ${className}`}>
       <div className="Table">
         <div className="Table-Row Table-Header">
-          { userFields.map((field, idx) => 
-              <div className="Table-Item" key={idx}>{field}</div>
-            ) }
+          {userFields.map((field, idx) =>
+            <div className="Table-Item" key={idx}>{field}</div>
+          )}
         </div>
-        { users.map((user) => { 
-            return (
-              <Link className="BlockLink BlockLink_primary" to={`/users/${user.u_id}`}><div className="Table-Row" key={user.u_id}>
-                <UserListRow user={user} />
-              </div></Link>
-            )
-          }) 
+        {users.map((user) => {
+          return (
+            <Link className="BlockLink BlockLink_primary" to={`/users/${user.u_id}`} key={user.u_id}><div className="Table-Row">
+              <UserListRow onDelete={onDelete} user={user} />
+            </div></Link>
+          )
+        })
         }
       </div>
     </div>
