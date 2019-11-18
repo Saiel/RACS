2b2a44fbfbc98fda0d12fd33fc5e152b877e4d1d
Alex Kale <alex-kale@github.com>
2019-11-18 15:03:41 +0300

    update styles

--- src/src/components/App/App.tsx
+++ src/src/components/App/App.tsx
@@ -9,12 +9,12 @@ import Header from 'components/Header/Header';
 import User from 'components/User/User';
 import Users from 'components/Users/Users';
 import Logs from 'components/Logs/Logs';
-import Accesses from 'components/Accesses/Accesses';
 import Locks from 'components/Locks/Locks';
+import Lock from 'components/Lock/Lock';
 
 const App = () => (
   <HashRouter>
-    <Header username="test test" userid={1} />
+    <Header />
     <Switch>
       <Route path="/" exact={true}>
         <Home />
@@ -22,6 +22,7 @@ const App = () => (
       <Route path="/users/:uId" component={User} />
       <Route path="/users/" component={Users} />
       <Route path="/logs/" component={Logs} />
+      <Route path="/locks/:lockId" component={Lock} />
       <Route path="/locks/" component={Locks} />
     </Switch>
   </HashRouter>
