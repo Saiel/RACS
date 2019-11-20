8101b278ac71c9c2f2bd4a1ab7435af87d48b297
Saiel <zaicevalex2000@gmail.com>
2019-11-20 16:15:12 +0300

    Added session authentication

--- src/lockadmin/views/web_views.py
+++ src/lockadmin/views/web_views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions, filters, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
-from guardian.shortcuts import get_objects_for_user, get_user_perms
+# from django_filters.rest_framework import DjangoFilterBackend
+# from guardian.shortcuts import get_objects_for_user, get_user_perms
 
 from ..serializers import *
 from ..models import *
@@ -13,9 +13,9 @@ class LocksViewSet(viewsets.ModelViewSet):
     queryset = Locks.objects.all()
     serializer_class = LocksSerializer
     permission_classes = [permissions.IsAdminUser]
+    # filter_backends = [filters.SearchFilter]
     # filterset_fields = ('description',)
-    filter_backends = [filters.SearchFilter]
-    search_fields = ('$description',)
+    # search_fields = ('$description',)
 
 
 class RolesViewSet(viewsets.ModelViewSet):
@@ -49,17 +49,15 @@ class UserModelViewSet(viewsets.ModelViewSet):
     serializer_class = UserModelSerializer
     permission_classes = [permissions.IsAdminUser]
 
-
-class UserInfo(generics.RetrieveAPIView):
-    queryset = UserModel.objects.all()
-    serializer_class = UserModelSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
     def retrieve(self, request, *args, **kwargs):
         instance = request.user
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+class LogsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Logs.objects.all()
+    serializer_class = LogsSerializer
+    permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
       queryset = Logs.objects.all()
