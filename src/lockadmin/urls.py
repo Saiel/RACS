7ef049aa8aef15e5c1d20297c4390ab8fe06af07
Alexey Zaytsev <alex@vm-34.staff.corp.local>
2019-11-25 12:56:24 +0000

    Added echo endpoint for locks

--- src/lockadmin/urls.py
+++ src/lockadmin/urls.py
@@ -24,10 +24,6 @@ locks_urls_patterns_v1 = [
 api_urls_patterns_v1 = [
     path('',               include(router.urls)),
     path('start-time/', views.get_server_start_time),
-]
-
-api_urls_patterns_v1 = [
-    path('',               include(router.urls)),
     path('get-user-info/', views.UserInfo.as_view()),
     path('token/auth/',    TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
