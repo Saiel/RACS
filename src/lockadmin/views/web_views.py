34997cf908e6f7fb9d1ad3594112f6b295f861b6
Saiel <zaicevalex2000@gmail.com>
2019-12-13 15:41:29 +0300

    Added server uptime endpoint

--- src/lockadmin/views/web_views.py
+++ src/lockadmin/views/web_views.py
@@ -1,7 +1,11 @@
+import os
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from django.conf import settings
+from rest_framework import viewsets, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 from ..serializers import *
 from ..models import *
@@ -69,3 +73,16 @@ class VueIndex(APIView):
     
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+
+@api_view(['GET'])
+def get_server_start_time(request):
+    result = {'datetime': ''}
+    try:
+        with open(os.path.join(settings.BASE_DIR, 'datestart.utcdatetime'), 'r') as ds_file:
+            result['datetime'] = ds_file.readline()
+            return Response(result, status=status.HTTP_200_OK)
+    except FileNotFoundError:
+        # Should not be here
+        result['Errors'] = 'Start file not found'
+        return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
